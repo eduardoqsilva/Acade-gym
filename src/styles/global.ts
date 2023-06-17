53c6acdde70affc3ac78ad73c1fe75a617f7d301
eduardoqsilva <eduadsilva@outlook.com>
2023-06-16 22:39:28 -0300

    update global styles :lipstick:

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
+import { DefaultTheme } from './theme/defaultTheme'
+
+const { font, colors } = DefaultTheme
 
 export const GlobalStyled = createGlobalStyle`
   *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${font};
+    color: ${colors.white};
   }
 `
