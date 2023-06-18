3cf3d8faee65f21d2f5d2dec538efb79eb3d294b
eduardoqsilva <eduadsilva@outlook.com>
2023-06-17 23:24:02 -0300

    create a default theme :lipstick:

---
+++ src/styles/theme/defaultTheme.ts
@@ -0,0 +1,37 @@
+export const DefaultTheme = {
+  font: {
+    default: 'Roboto, sans-serif',
+  },
+  fontSize: {
+    XL: '1.5rem',
+    L: '1.25rem',
+    M: '1rem',
+    S: '0.875rem',
+    XS: '0.75rem',
+  },
+  border: {
+    borderRadius: '8px',
+    borderWidth: '2px',
+  },
+  colors: {
+    orange: '#EB5E28',
+    orange_light: '#FF7541',
+    orange_dark: '#783116',
+
+    red: '#F03847',
+    red_dark: '#7A1921',
+
+    green_light: '#8FF7A7',
+    yellow: '#F2C14E',
+
+    gray1: '#121214',
+    gray2: '#202024',
+    gray3: '#323238',
+    gray4: '#7C7C8A',
+    gray5: '#8D8D99',
+    gray6: '#C4C4CC',
+    gray7: '#E1E1E6',
+
+    white: '#FFFF',
+  },
+} as const
