881bf6686f4b4d46955d4f959899eb91b3091332
eduardoqsilva <eduadsilva@outlook.com>
2023-06-22 23:32:51 -0300

    create a component SuccessIndicator :white_check_mark:

---
+++ src/components/SuccessIndicator/index.tsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react'
+import { DefaultTheme } from '../../styles/theme/defaultTheme'
+import { ContainerStyled, LoadingDivStyled } from './successIndicator.styled'
+import { useNavigate } from 'react-router-dom'
+import { SuccessIndicatorType } from './sucessIndicator'
+import {
+  CheckCircle,
+  CircleNotch,
+  IconContext,
+  XCircle,
+} from '@phosphor-icons/react'
+
+const { colors } = DefaultTheme
+
+export function SuccessIndicator({
+  indicate /* success / show */,
+  redirect,
+}: SuccessIndicatorType) {
+  const color = indicate[0] ? colors.green_light : colors.red
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    let timeOut: number
+
+    if (redirect && indicate[0] && indicate[1]) {
+      timeOut = setTimeout(() => {
+        navigate(redirect)
+      }, 1500)
+    }
+
+    return () => clearTimeout(timeOut)
+  }, [indicate, navigate, redirect])
+
+  return (
+    <ContainerStyled show={indicate[1]}>
+      <div className="indicator">
+        <IconContext.Provider
+          value={{
+            color: `${color}`,
+            size: 100,
+            weight: 'regular',
+          }}
+        >
+          {indicate[0] !== null ? (
+            indicate[0] ? (
+              <CheckCircle className="animate" />
+            ) : (
+              <XCircle className="animate" />
+            )
+          ) : (
+            <LoadingDivStyled>
+              <CircleNotch color={colors.white} />
+            </LoadingDivStyled>
+          )}
+        </IconContext.Provider>
+
+        <h2>
+          {indicate[0] !== null
+            ? indicate[0]
+              ? 'Sucesso'
+              : 'Ocorreu um erro!'
+            : 'Aguarde!'}
+        </h2>
+      </div>
+    </ContainerStyled>
+  )
+}
---
+++ src/components/SuccessIndicator/sucessIndicator.d.ts
@@ -0,0 +1,4 @@
+export interface SuccessIndicatorType {
+  indicate: [success: boolean | null, show: boolean] // success show
+  redirect?: string
+}
