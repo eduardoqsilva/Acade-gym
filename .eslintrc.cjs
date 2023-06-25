e28ae94268a6598d02a7229254d8ae702efa7e50
eduardoqsilva <eduadsilva@outlook.com>
2023-06-25 20:59:58 -0300

    update dependences add storybook :arrow_up:

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     '@rocketseat/eslint-config/react',
+    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
