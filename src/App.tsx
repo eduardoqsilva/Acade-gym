58ec67bf4a67bd54e8dbe13ec270cce430e7b29d
eduardoqsilva <eduadsilva@outlook.com>
2023-06-26 17:35:53 -0300

    move global styles from main.tsx :lipstick:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routes/Router'
-import { GlobalStyled } from './styles/global'
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyled />
       <Router />
     </BrowserRouter>
   )
