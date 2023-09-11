c1a6818d051ef948f05e2a2424d35895931def42
eduardoqsilva <eduadsilva@outlook.com>
2023-09-10 23:26:41 -0300

    add a themeProvider :lipstick:

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { DefaultTheme } from './styles/theme/defaultTheme.ts'
 import { GlobalStyled } from './styles/global.styled.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyled />
-    <App />
+    <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyled />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
