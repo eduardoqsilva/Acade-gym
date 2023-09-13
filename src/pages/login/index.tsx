c48521777e0717053c12688cbee04603c14bc131
eduardoqsilva <eduadsilva@outlook.com>
2023-09-13 00:41:24 -0300

    create a login page UI and login page styles :lipstick:

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,40 @@
+import { Input } from '../../components/Inputs'
+import { At, LockKey } from '@phosphor-icons/react'
+import { Logo } from '../../components/Logo'
+import { ContainerStyled, FormStyled, WrapperStyled } from './login.styled'
+import { Button } from '../../components/Button'
+
 export function Login() {
   return (
-    <>
-      <h2>login</h2>
-    </>
+    <ContainerStyled>
+      <WrapperStyled>
+        <Logo width={120} />
+        <FormStyled>
+          <h2 className="title">Login</h2>
+          <Input
+            icon={<At />}
+            type={'email'}
+            label={'E-mail:'}
+            placeholder="seuemail@email.com"
+            warning={false}
+            msgError={''}
+            variation="02"
+          />
+          <Input
+            icon={<LockKey />}
+            type={'password'}
+            label={'Senha:'}
+            placeholder="••••••••••••"
+            warning={false}
+            msgError={''}
+            variation="02"
+          />
+          <div className="buttonsWrapper">
+            <Button text={'Entrar'} type="submit" />
+            <Button text={'Cadastrar'} variation="secundary" type="button" />
+          </div>
+        </FormStyled>
+      </WrapperStyled>
+    </ContainerStyled>
   )
 }
