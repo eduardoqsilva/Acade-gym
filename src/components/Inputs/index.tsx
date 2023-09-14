11ac9418efa8397f3ae4eaab0f9336f579534570
eduardoqsilva <eduadsilva@outlook.com>
2023-09-13 21:32:14 -0300

    refactor all inputs and types :recycle:

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Code, IconContext } from '@phosphor-icons/react'
 import {
   CheckBoxStyled,
@@ -6,14 +6,13 @@ import {
   ErrorTextStyled,
   InputWrapperStyled,
 } from './inputs.styled'
-import { CheckBoxType, DropdownInputType, TextInputType } from './inputsType'
+import { CheckboxType, DropdownType, InputType } from './inputsType'
 
-export const Input = React.forwardRef<HTMLInputElement, TextInputType>(
+export const Input = React.forwardRef<HTMLInputElement, InputType>(
   function Input(
     {
       type = 'text',
       label,
-      warning,
       msgError,
       variation = '01',
       icon = <Code />,
@@ -21,6 +20,16 @@ export const Input = React.forwardRef<HTMLInputElement, TextInputType>(
     },
     ref,
   ) {
+    const [warning, setWarning] = useState(false)
+
+    useEffect(() => {
+      if (msgError) {
+        setWarning(true)
+      } else {
+        setWarning(false)
+      }
+    }, [msgError])
+
     return (
       <div>
         <InputWrapperStyled variation={variation} warning={warning}>
@@ -42,48 +51,59 @@ export const Input = React.forwardRef<HTMLInputElement, TextInputType>(
     )
   },
 )
+export const Dropdown = React.forwardRef<HTMLSelectElement, DropdownType>(
+  function Dropdown(
+    { icon = <Code />, label, itens, msgError, ...props },
+    ref,
+  ) {
+    const [warning, setWarning] = useState(false)
 
-export function Dropdown({
-  icon = <Code />,
-  label,
-  required,
-  itens,
-  warning,
-}: DropdownInputType) {
-  return (
-    <div>
-      <DropdownStyled warning={warning}>
-        <IconContext.Provider
-          value={{
-            color: 'currentColor',
-            size: 25,
-            weight: 'regular',
-          }}
-        >
-          {icon}
-        </IconContext.Provider>
+    useEffect(() => {
+      if (msgError) {
+        setWarning(true)
+      } else {
+        setWarning(false)
+      }
+    }, [msgError])
 
-        <label>{label}</label>
-        <select name="sexo" required={required}>
-          <option value="">--</option>
-          {itens.map((i, id) => {
-            return (
-              <option key={id} value={i}>
-                {i}
-              </option>
-            )
-          })}
-        </select>
-      </DropdownStyled>
-    </div>
-  )
-}
+    return (
+      <div>
+        <DropdownStyled warning={warning}>
+          <IconContext.Provider
+            value={{
+              color: 'currentColor',
+              size: 25,
+              weight: 'regular',
+            }}
+          >
+            {icon}
+          </IconContext.Provider>
 
-export function CheckBox({ text, name }: CheckBoxType) {
-  return (
-    <CheckBoxStyled>
-      <input type="checkbox" id={name} />
-      <label htmlFor={name}>{text}</label>
-    </CheckBoxStyled>
-  )
-}
+          <label>{label}</label>
+          <select ref={ref} {...props}>
+            <option value="">--</option>
+            {itens.map((i, id) => {
+              return (
+                <option key={id} value={i}>
+                  {i}
+                </option>
+              )
+            })}
+          </select>
+        </DropdownStyled>
+        <ErrorTextStyled warning={warning}>{msgError}</ErrorTextStyled>
+      </div>
+    )
+  },
+)
+
+export const CheckBox = React.forwardRef<HTMLInputElement, CheckboxType>(
+  function CheckBox({ text, name }, ref) {
+    return (
+      <CheckBoxStyled>
+        <input ref={ref} type="checkbox" id={name} />
+        <label htmlFor={name}>{text}</label>
+      </CheckBoxStyled>
+    )
+  },
+)
