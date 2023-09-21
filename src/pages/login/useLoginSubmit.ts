b0643031f4d58c7f138d367f8e6c7f5be08a3367
eduardoqsilva <eduadsilva@outlook.com>
2023-09-21 00:25:30 -0300

    set user in auth context

--- src/pages/login/useLoginSubmit.ts
+++ src/pages/login/useLoginSubmit.ts
@@ -1,20 +1,34 @@
+import { useContext } from 'react'
 import { Api } from '../../services/api/axiosInstance'
-import { FormProps, LoginPost, LoginSubmitProps } from './types'
+import { FormProps, LoginPost, LoginResponse, LoginSubmitProps } from './types'
+import { AuthContext } from '../../contexts/auth/AuthContext'
+import { AxiosResponse } from 'axios'
 
 export function useLoginSubmit({
   changeModalState,
   openModal,
   setModalErrorMsg,
 }: LoginSubmitProps) {
+  const { login } = useContext(AuthContext)
+
   function loginSubmit(data: FormProps) {
     openModal()
     changeModalState('loading')
-    Api.post<LoginPost>('/login', {
+
+    Api.post<LoginPost, AxiosResponse<LoginResponse>>('/login', {
       email: data.login.email,
       password: data.login.password,
     })
-      .then((data) => {
-        console.log(data)
+      .then((response) => {
+        const data = response.data
+        const user = {
+          accessToken: data.accessToken,
+          user: {
+            email: data.user.email,
+            id: data.user.id,
+          },
+        }
+        login(user)
         changeModalState('success')
       })
       .catch((err) => {
