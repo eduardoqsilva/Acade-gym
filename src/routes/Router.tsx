1d855856889e221d160051ce41f824f3df310798
eduardoqsilva <eduadsilva@outlook.com>
2023-09-26 01:22:07 -0300

    create a route /register

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,12 +3,14 @@ import { Home } from '../pages/home'
 import { Login } from '../pages/login'
 import { useContext } from 'react'
 import { AuthContext } from '../contexts/auth/AuthContext'
+import { Register } from '../pages/register'
 
 export function Router() {
   const { isLogged } = useContext(AuthContext)
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/home" element={isLogged ? <Home /> : <Login />} />
     </Routes>
   )
