93fd207f49cafa53a517ee0ae695d017022c850b
eduardoqsilva <eduadsilva@outlook.com>
2023-09-26 00:46:55 -0300

    configure a react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import App from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 import { DefaultTheme } from './styles/theme/defaultTheme.ts'
 import { GlobalStyled } from './styles/global.styled.ts'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={DefaultTheme}>
       <GlobalStyled />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
